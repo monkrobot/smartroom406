98784b76677e39939d647655a6ba0aa3b1134f39
monkrobot <sdenisov93@mail.ru>
2017-07-31 16:29:04 +0300

    ESP-32 - Wi-Fi - wifi_standard.py - rewrite all file, old realization - wifi_standard_bad_realize.py

--- ESP-32/Wi-Fi/wifi_standard.py
+++ ESP-32/Wi-Fi/wifi_standard.py
@@ -1,5 +1,13 @@
-'''Class for determining the wi-fi standard and counting number of wi-fi devices use one of the standards'''
+'''Class for determining the wi-fi standard'''
 
+import json
+from pprint import pprint
+
+with open('prot_wifi.json') as data_file:
+    wifi_prots = json.load(data_file)
+
+pprint(wifi_prots)
+print('\n')
 
 class WifiStandard:
     '''Parent-class for determing the wi-fi standard'''
@@ -9,87 +17,12 @@ class WifiStandard:
         self.signal = signal
 
 
-class ProtOld802_11(WifiStandard):
-    '''Class for standard 802.11'''
-    numDev = 0
-
-    def __init__(self, device):
-        super().__init__(2, [2.4], ['FHSS', 'DSSS'])
-        self.device = device
-        ProtOld802_11.numDev += 1
+class Device(WifiStandard):
+    def __init__(self, prot):
+        self.prot = wifi_prots[prot]
+        super().__init__(self.prot["maxSpeed"], wifi_prots[prot]["freq"], wifi_prots[prot]["signal"])
 
 
-class Prot802_11a(WifiStandard):
-    '''Class for standard 802.11a'''
-    numDev = 0
-
-    def __init__(self, device):
-        super().__init__(54, [5], ['OFDM'])
-        self.device = device
-        Prot802_11a.numDev += 1
-
-
-class Prot802_11b(WifiStandard):
-    '''Class for standard 802.11b'''
-    numDev = 0
-
-    def __init__(self, device):
-        super().__init__(11, [2.4], ['HR-DSSS'])
-        self.device = device
-        Prot802_11b.numDev += 1
-
-
-class Prot802_11g(WifiStandard):
-    '''Class for standard 802.11g'''
-    numDev = 0
-
-    def __init__(self, device):
-        super().__init__(54, [2.4], ['OFDM'])
-        self.device = device
-        Prot802_11g.numDev += 1
-
-
-class Prot802_11n(WifiStandard):
-    '''Class for standard 802.11n'''
-    numDev = 0
-
-    def __init__(self, device):
-        super().__init__(600, [2.4, 5], ['OFDM'])
-        self.device = device
-        Prot802_11n.numDev += 1
-
-
-class Prot802_11ac(WifiStandard):
-    '''Class for standard 802.11ac'''
-    numDev = 0
-    def __init__(self, device):
-        super().__init__(1300, [5], ['256-QAM'])
-        self.device = device
-        Prot802_11ac.numDev += 1
-
-
-if __name__ == '__main__':
-    prot_1 = ProtOld802_11('THis')
-    prot_2 = ProtOld802_11('THis')
-    prot_3 = ProtOld802_11('THis')
-    print('Frequency of wi-fi for 802.11: ', prot_2.freq)
-    print('802.11: ', ProtOld802_11.numDev)
-
-    device11a_1 = Prot802_11a('mobile')
-    print('802.11a: ', Prot802_11a.numDev)
-
-    device11b_1 = Prot802_11b('mobile')
-    print('802.11b: ', Prot802_11b.numDev)
-
-    device11g_1 = Prot802_11g('laptop')
-    device11g_2 = Prot802_11g('camera')
-    print('802.11g: ', Prot802_11g.numDev)
-
-    device11n_1 = Prot802_11n('camera1')
-    device11n_2 = Prot802_11n('camera2')
-    device11n_3 = Prot802_11n('camera3')
-    print('802.11n: ', Prot802_11n.numDev)
-
-    print('802.11ac: ', Prot802_11ac.numDev)
-
-print('doc: ', help(Prot802_11ac))
+device = Device('prot802_11b')
+print('Device wifi protocol: ', device.prot)
+print("speed: ", device.maxSpeed)
