302ade4b584dd0c9e84cd4f49180a9d0661d19fa
monkrobot <sdenisov93@mail.ru>
2017-08-15 16:20:26 +0300

    ESP-32 - map - map.py

--- map/map.py
+++ map/map.py
@@ -1,89 +1,45 @@
-#import matplotlib.pyplot as plt
-#
-#class Room:
-#    def __init__(self, long, width, height, points=[]):
-#        self.long = long
-#        self.width = width
-#        self.height = height
-#        self.points = points
-
-#import numpy as np
-#import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
-#
-#fig = plt.figure()
-#ax = Axes3D(fig)
-#X = np.arange(-4, 4, 0.25)
-#Y = np.arange(-4, 4, 0.25)
-#X, Y = np.meshgrid(X, Y)
-#R = np.sqrt(X**2 + Y**2)
-#Z = np.sin(R)
-#
-#ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='hot')
-#
-#plt.show()
-
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
-from itertools import product, combinations
+
+class Room:
+    def __init__(self, long, width, points=[0,0]):
+        self.long = long
+        self.width = width
+        #self.height = height
+        self.points = points
+
+    def drawingRoom(self):
+        fig2 = plt.figure()
+        ax2 = fig2.add_subplot(111, aspect='equal')
+        ax2.add_patch(
+            patches.Rectangle(
+                (0.1, 0.1),
+                self.width,
+                self.long,
+                fill=False  # remove background
+            )
+        )
+        ax2.add_patch(
+            patches.Circle(
+                (self.points),
+                radius=0.3,
+                edgecolor="red",
+                fill=False
+            )
+        )
+        ax2.add_patch(
+            patches.Circle(
+                (self.points),
+                radius=0.01,
+                edgecolor="red",
+                fill=False
+            )
+        )
+
+        plt.show()
 
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.set_aspect("equal")
+testRoom = Room(0.8, 0.5, points=[0.2, 0.4])
+testRoom.drawingRoom()
 
-
-# draw cube
-length = [-3, 3]
-width = [-1.5,1.5]
-height = [-2,2]
-#cube = [[0,0,length,length,0,0,length,length],[0,width,0,width,0,width,0,width],[0,0,0,0,height,height,height,height]]
-#cube = [[0,0,0],[0,width,0],[length,0,0],[length,width,0],[0,0,height],[0,width,height],
-#        [length,0,height],[length,width,height]]
-
-#for len,wid in combinations(np.array(list(product(length,width,height))),2):
-#    if np.sum(np.abs(len - wid)) != 0:
-#        ax.plot3D(*zip(len, wid), color="b")
-
-
-
-
-
-#r = [-1, 1]
-#print(list(combinations(np.array(list(product(r, r, r))), 2)))
-#for s, e in combinations(np.array(list(product(r, r, r))), 2):
-#    if np.sum(np.abs(s-e)) == r[1]-r[0]:
-#        ax.plot3D(*zip(s, e), color="b")
-
-## draw sphere
-#u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-#x = np.cos(u)*np.sin(v)
-#y = np.sin(u)*np.sin(v)
-#z = np.cos(v)
-#ax.plot_wireframe(x, y, z, color="r")
-#
-## draw a point
-#ax.scatter([0], [0], [0], color="g", s=100)
-#
-## draw a vector
-#from matplotlib.patches import FancyArrowPatch
-#from mpl_toolkits.mplot3d import proj3d
-#
-#
-#class Arrow3D(FancyArrowPatch):
-#
-#    def __init__(self, xs, ys, zs, *args, **kwargs):
-#        FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
-#        self._verts3d = xs, ys, zs
-#
-#    def draw(self, renderer):
-#        xs3d, ys3d, zs3d = self._verts3d
-#        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
-#        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
-#        FancyArrowPatch.draw(self, renderer)
-#
-#a = Arrow3D([0, 1], [0, 1], [0, 1], mutation_scale=20,
-#            lw=1, arrowstyle="-|>", color="k")
-#ax.add_artist(a)
-plt.show()
