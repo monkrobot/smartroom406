ef06ef24fa861b71a77c6618eeebfeb8b894102d
monkrobot <sdenisov93@mail.ru>
2017-09-14 17:00:01 +0300

    ESP-32 - Wi-Fi - device.py - wifi speed for different distances

--- map/map_testPYQT.py
+++ map/map_testPYQT.py
@@ -14,7 +14,12 @@ class Map(QWidget):
 
 
 
-    elHght, elWdth = 0, 0 #Mouse click WiFi zone
+    elHght_72, elWdth_72 = 0, 0 #Mouse click WiFi zone
+    elHght_54, elWdth_54 = 0, 0
+    elHght_32, elWdth_32 = 0, 0
+    elHght_11, elWdth_11 = 0, 0
+    elHght_6, elWdth_6 = 0, 0
+    elHght_1, elWdth_1 = 0, 0
     elCntH, elCntW = 0, 0 #Mouse click WiFi zone center
     clickNum = 0 #Number of mouse click for device
     devList = [] #Device list
@@ -26,12 +31,34 @@ class Map(QWidget):
 
         painter = QPainter(self._im)
         painter.setPen(QPen(QColor(Map.elClr), 2, Qt.SolidLine, Qt.RoundCap))
-        painter.setBrush(QBrush(QColor(Map.elClr), Qt.CrossPattern))
-
-        painter.drawEllipse(point, Map.elHght, Map.elWdth)
+        painter.setBrush(QBrush(QColor(Map.elClr), Qt.SolidPattern))
         painter.drawEllipse(point, Map.elCntH, Map.elCntW)
 
-        if Map.elHght >= 200:
+        painter.setPen(QPen(QColor("#000"), 2, Qt.SolidLine, Qt.RoundCap))
+        painter.setBrush(QBrush(QColor("#000"), Qt.CrossPattern))
+        painter.drawEllipse(point, Map.elHght_72, Map.elWdth_72)
+
+        painter.setPen(QPen(QColor("#439232"), 2, Qt.SolidLine, Qt.RoundCap))
+        painter.setBrush(QBrush(QColor("#439232"), Qt.CrossPattern))
+        painter.drawEllipse(point, Map.elHght_54, Map.elWdth_54)
+
+        painter.setPen(QPen(QColor("#923243"), 2, Qt.SolidLine, Qt.RoundCap))
+        painter.setBrush(QBrush(QColor("#923243"), Qt.CrossPattern))
+        painter.drawEllipse(point, Map.elHght_32, Map.elWdth_32)
+
+        painter.setPen(QPen(QColor("#324392"), 2, Qt.SolidLine, Qt.RoundCap))
+        painter.setBrush(QBrush(QColor("#324392"), Qt.CrossPattern))
+        painter.drawEllipse(point, Map.elHght_11, Map.elWdth_11)
+
+        painter.setPen(QPen(QColor("#324392"), 2, Qt.SolidLine, Qt.RoundCap))
+        painter.setBrush(QBrush(QColor("#324392"), Qt.CrossPattern))
+        painter.drawEllipse(point, Map.elHght_6, Map.elWdth_6)
+
+        painter.setPen(QPen(QColor("#324392"), 2, Qt.SolidLine, Qt.RoundCap))
+        painter.setBrush(QBrush(QColor("#324392"), Qt.CrossPattern))
+        painter.drawEllipse(point, Map.elHght_1, Map.elWdth_1)
+
+        if Map.elHght_72 >= 10:
 
             painter.setPen(QPen(QColor("#000000")))
             painter.setFont(QFont('Arial', 15))
@@ -46,7 +73,7 @@ class Map(QWidget):
             print("Y is", pointPressY)
 
             #New device
-            dev = device.Device([pointPressX, pointPressY], Map.elHght)
+            dev = device.Device([pointPressX, pointPressY], Map.elHght_72)
 
             #Device list
             Map.devList.append(dev)
@@ -97,7 +124,49 @@ class Map(QWidget):
         #Painting of room - rectangle
         painterRect = QPainter(self._im)
         painterRect.setPen(QPen(QColor("#000"), 1, Qt.SolidLine, Qt.RoundCap))
-        painterRect.drawRect(100, 100, 1000, 800)
+        painterRect.drawRect(100, 100, 100, 800)
+
+        ##History
+#
+        ## Size and color for drawing zone
+        #self._in = QImage(1700, 960, QImage.Format_ARGB32)
+        #self._in.fill(QColor("red"))
+        #
+        ##Color for speed 72Mbps
+        #history = QPainter(self._in)
+        #history.setPen(QPen(QColor("#439232"), 1, Qt.SolidLine, Qt.RoundCap))
+        ##history_72.setBrush(QBrush(QColor("#439232"), Qt.CrossPattern))
+        #history.drawRect(400, 100, 100, 800)
+#
+        ## Color for speed 54Mbps
+        #history_54 = QPainter(self._im)
+        #history_54.setPen(QPen(QColor("#923243"), 1, Qt.SolidLine, Qt.RoundCap))
+        #history_54.setBrush(QBrush(QColor("#923243"), Qt.CrossPattern))
+        #history_54.drawRect(50, 150, 50, 50)
+#
+        ## Color for speed 32Mbps
+        #history_32 = QPainter(self._im)
+        #history_32.setPen(QPen(QColor("#324392"), 1, Qt.SolidLine, Qt.RoundCap))
+        #history_32.setBrush(QBrush(QColor("#324392"), Qt.CrossPattern))
+        #history_32.drawRect(50, 200, 50, 50)
+#
+        ## Color for speed 11Mbps
+        #history_11 = QPainter(self._im)
+        #history_11.setPen(QPen(QColor("#000"), 1, Qt.SolidLine, Qt.RoundCap))
+        #history_11.setBrush(QBrush(QColor("#000"), Qt.CrossPattern))
+        #history_11.drawRect(50, 250, 50, 50)
+#
+        ## Color for speed 6Mbps
+        #history_6 = QPainter(self._im)
+        #history_6.setPen(QPen(QColor("#000"), 1, Qt.SolidLine, Qt.RoundCap))
+        #history_6.setBrush(QBrush(QColor("#000"), Qt.CrossPattern))
+        #history_6.drawRect(50, 300, 50, 50)
+#
+        ## Color for speed 1Mbps
+        #history_1 = QPainter(self._im)
+        #history_1.setPen(QPen(QColor("#000"), 1, Qt.SolidLine, Qt.RoundCap))
+        #history_1.setBrush(QBrush(QColor("#000"), Qt.CrossPattern))
+        #history_1.drawRect(50, 350, 50, 50)
 
         #Size and Title for program window
         self.setFixedSize(1700, 960)
@@ -110,7 +179,12 @@ class Map(QWidget):
         Function for button Router
         Changes size and color of router's wifi zone
         '''
-        Map.elHght, Map.elWdth = 300, 300
+        Map.elHght_72, Map.elWdth_72 = 300, 300
+        #Map.elHght_54, Map.elWdth_54
+        #Map.elHght_32, Map.elWdth_32
+        #Map.elHght_11, Map.elWdth_11
+        #Map.elHght_6, Map.elWdth_6
+        #Map.elHght_1, Map.elWdth_1
         Map.elCntH, Map.elCntW = 20, 20
         Map.elClr = "#431292"
         print("Button Router pressed")
@@ -120,8 +194,13 @@ class Map(QWidget):
         Function for button ESP-32
         Changes size and color of esp's wifi zone
         '''
-        Map.elHght, Map.elWdth = 200, 200
-        Map.elCntH, Map.elCntW = 10, 10
+        Map.elHght_72, Map.elWdth_72 = 15, 15
+        Map.elHght_54, Map.elWdth_54 = 24, 24
+        Map.elHght_32, Map.elWdth_32 = 41, 41
+        Map.elHght_11, Map.elWdth_11 = 155, 155
+        Map.elHght_6, Map.elWdth_6 = 196, 196
+        Map.elHght_1, Map.elWdth_1 = 348, 348
+        Map.elCntH, Map.elCntW = 5, 5
         Map.elClr = "#439232"
         print("Button ESP-32 pressed")
 
@@ -130,7 +209,12 @@ class Map(QWidget):
         Function for button Stop
         Changes size of wifi zone to 0
         '''
-        Map.elHght, Map.elWdth = 0, 0
+        Map.elHght_72, Map.elWdth_72 = 0, 0
+        Map.elHght_54, Map.elWdth_54 = 0, 0
+        Map.elHght_32, Map.elWdth_32 = 0, 0
+        Map.elHght_11, Map.elWdth_11 = 0, 0
+        Map.elHght_6, Map.elWdth_6 = 0, 0
+        Map.elHght_1, Map.elWdth_1 = 0, 0
         Map.elCntH, Map.elCntW = 0, 0
         print("Button Stop pressed")
         print("Fuck you!")
